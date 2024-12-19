869a798b879706d15c4f7d2ca78d020300b5cf6b
DavidNaranjoG <david.naranjo@corraounivalle.edu.co>
2024-12-18 22:45:29 -0500

    se agregaron los componentes de logi, lista de contactos, registro, y editar contacto

---
+++ contactBook/src/app/components/editar-contacto/editar-contacto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-contacto',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-contacto.component.html',
+  styleUrl: './editar-contacto.component.css'
+})
+export class EditarContactoComponent {
+
+}
---
+++ contactBook/src/app/components/lista-contactos/lista-contactos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-contactos',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-contactos.component.html',
+  styleUrl: './lista-contactos.component.css'
+})
+export class ListaContactosComponent {
+
+}
